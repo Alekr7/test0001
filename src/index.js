1972408037012f8a31630c2a61e9379227042ddb
Alekr7 <flohlvsave@gmail.com>
2025-08-30 16:18:47 +0200

    deploy: publish build to /docs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // BrowserRouter = routeur SPA (→ navigation côté client)
 import './index.css';
 import App from './App';
-import './i18n';
+import './i18n'; // i18n = internationalisation (chargement des traductions)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> {/* StrictMode = garde-fous dev, pas utilisé en prod */}
+    <BrowserRouter basename="/test0001"> {/* basename = préfixe d’URL sous GitHub Pages */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
